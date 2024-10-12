b478b70bc4823d9c80fa8e3e96e10d3ef3988873
Kazumasa Utashiro <kaz@utashiro.com>
2024-10-12 21:33:08 +0900

    minimum update

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 
 # NAME
 
-App::Greple::under - It's new $module
+App::Greple::under - greple under module
 
 # SYNOPSIS
 
-    use App::Greple::under;
+    greple -Munder
 
 # DESCRIPTION
 
-App::Greple::under is ...
-
-# LICENSE
-
-Copyright (C) Kazumasa Utashiro.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+**greple**'s **under** module emphasizes matched text not in the same line
+but in the next line.
 
 # AUTHOR
 
-Kazumasa Utashiro <kaz@utashiro.com>
+Kazumasa Utashiro
+
+# LICENSE
+
+Copyright ©︎ 2024 Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
--- lib/App/Greple/under.pm
+++ lib/App/Greple/under.pm
@@ -1,39 +1,38 @@
 package App::Greple::under;
-use 5.008001;
-use strict;
+use 5.024;
 use warnings;
 
 our $VERSION = "0.01";
 
-
-
 1;
+
 __END__
 
 =encoding utf-8
 
 =head1 NAME
 
-App::Greple::under - It's new $module
+App::Greple::under - greple under module
 
 =head1 SYNOPSIS
 
-    use App::Greple::under;
+    greple -Munder
 
 =head1 DESCRIPTION
 
-App::Greple::under is ...
-
-=head1 LICENSE
-
-Copyright (C) Kazumasa Utashiro.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+B<greple>'s B<under> module emphasizes matched text not in the same line
+but in the next line.
 
 =head1 AUTHOR
 
-Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+Kazumasa Utashiro
+
+=head1 LICENSE
+
+Copyright ©︎ 2024 Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
 
