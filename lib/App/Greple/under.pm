76dc079bbe30198c0dae5941a6bcc7e14db86525
Kazumasa Utashiro <kaz@utashiro.com>
2025-06-12 22:16:24 +0900

    Checking in changes prior to tagging of version 1.00.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index eb1552e..e3a0f00 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension App-Greple-under
     
     {{$NEXT}}
     
    +1.00 2025-06-12T13:16:19Z
    +
    +    - use Getopt::EX::Config
    +
     0.9905 2024-10-19T09:53:47Z
     
         - sequence parameter accept combining character

--- lib/App/Greple/under.pm
+++ lib/App/Greple/under.pm
@@ -2,7 +2,7 @@ package App::Greple::under;
 use 5.024;
 use warnings;
 
-our $VERSION = "0.9905";
+our $VERSION = "1.00";
 
 =encoding utf-8
 
@@ -106,7 +106,7 @@ Kazumasa Utashiro
 
 =head1 LICENSE
 
-Copyright ©︎ 2024 Kazumasa Utashiro.
+Copyright ©︎ 2024-2025 Kazumasa Utashiro.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
